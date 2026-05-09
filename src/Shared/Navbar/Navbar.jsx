8de2f52e89fd0b0692aaaf2a132c9be23281e652
David Chijioke <Daveedchijioke@gmail.com>
2026-05-09 20:29:59 +0100

    Create navbar component structure

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,11 @@
-import React, { use } from 'react';
 import { useState } from 'react';
 import './Navbar.scss'
-
-
+import { FaMoon, FaSun} from "react-icons/fa";
 
 
 
 function Navbar({theme, toggleTheme}){
-    cosnt [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const links = ["Home", "About", "Skills", "Projects", "Contact"];
 
 
@@ -15,24 +13,24 @@ function Navbar({theme, toggleTheme}){
 
 
     return(
-        <nav className='nav-bar'>
-            <div className='nav__logo'>DC.</div>
+        <nav className='navbar'>
+            <div className='navbar__logo'>DC.</div>
 
-            {/*Desktop links */}
-            <ul>
+            {/*Desktop links and mobile links */}
+            <ul className= {`navbar__links ${isOpen ? "navbar__links--open" : ""}`}>
                 {links.map((items, index)=>{
-                    return <li key={index}><a href="">{items}</a></li>
+                    return <li key={index} className='navbar__item'><a href="" className='navbar__link'>{items}</a></li>
                 })}
             </ul>
-
+               
             {/* Right side controls */}
             <div className='navbar__controls'>
 
               <button className='navbar__theme-toggle' onClick={toggleTheme}> 
-                {theme === "dark" ? "☀️" : "🌙"}   
+                {theme === "dark" ? <FaSun /> : <FaMoon />}   
               </button>
 
-              <button className={`navbar__hamburger ${isOpen ? "Open" : ""}`} onClick={()=> setIsOpen(!isOpen)}>
+              <button className={`navbar__hamburger ${isOpen ? "open" : ""}`} onClick={()=> setIsOpen(!isOpen)}>
 
                 <span></span>
                 <span></span>
@@ -42,12 +40,7 @@ function Navbar({theme, toggleTheme}){
 
             </div>
 
-            {/*Mobile links*/}
-            <ul>
-                {links.map((items, index)=>{
-                    return <li key={index}><a href="">{items}</a></li>
-                })}
-            </ul>
+            
 
 
         </nav>
